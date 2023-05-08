bed60503ff15fc67b70e74d6c2e9b949caa7d324
Yara Horany <yara.horany1@gmail.com>
2023-05-08 19:57:06 +0300

    Adding constants colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = const Color(0xFF14BDAC);
+  static Color secondaryColor = const Color(0xFFcc66ff);
+  static Color accentColor = const Color(0xFF1a75ff);
+}
