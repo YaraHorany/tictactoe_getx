341425621448f4722d6a209ecefa50f58f200db4
Yara Horany <yara.horany1@gmail.com>
2023-05-08 20:35:11 +0300

    Implementing tapped button

--- lib/controllers/tictactoe_controller.dart
+++ lib/controllers/tictactoe_controller.dart
@@ -4,6 +4,27 @@ import 'package:tictactoe_getx/models/tictactoe.dart';
 class TicTacToeController extends GetxController {
   final ticTacToe = TicTacToe().obs;
 
+  void tapped(int index) {
+    if (!ticTacToe.value.winnerFound && ticTacToe.value.attempts != 0) {
+      if (ticTacToe.value.oTurn && ticTacToe.value.displayXO[index] == '') {
+        ticTacToe.update((val) {
+          val?.displayXO[index] = 'O';
+          val?.filledBoxes++;
+          val?.oTurn = !ticTacToe.value.oTurn;
+          val?.checkWinner();
+        });
+      } else if (!ticTacToe.value.oTurn &&
+          ticTacToe.value.displayXO[index] == '') {
+        ticTacToe.update((val) {
+          val?.displayXO[index] = 'X';
+          val?.filledBoxes++;
+          val?.oTurn = !ticTacToe.value.oTurn;
+          val?.checkWinner();
+        });
+      }
+    }
+  }
+
   void clearBoard() {
     ticTacToe.update((val) {
       for (int i = 0; i < 9; i++) {
