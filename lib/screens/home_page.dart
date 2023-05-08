f1d1f5426e40655e595dcfe7bc586205667174f9
Yara Horany <yara.horany1@gmail.com>
2023-05-08 20:17:27 +0300

    Adding header to the home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tictactoe_getx/constants/colors.dart';
 
 class HomePage extends StatelessWidget {
+  static var customFontWhite = GoogleFonts.coiny(
+    textStyle: const TextStyle(
+      color: Colors.white,
+      letterSpacing: 3,
+      fontSize: 28,
+    ),
+  );
+
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: MainColor.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('Player X', style: customFontWhite),
+                          Text('0', style: customFontWhite),
+                        ],
+                      ),
+                      const SizedBox(width: 20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('Player O', style: customFontWhite),
+                          Text('0', style: customFontWhite),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
